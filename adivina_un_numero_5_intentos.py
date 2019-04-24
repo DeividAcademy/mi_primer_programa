437fc864aa748fe5dc2b1239c81d5a734156ae62
DeiviAcademy <deiviopcional2@gmail.com>
2019-04-24 17:31:36 +0200

    Adivina un numero 5 intentos,Bien hecho

---
+++ adivina_un_numero_5_intentos.py
@@ -0,0 +1,26 @@
+
+number_to_guess=2
+user_number=int(input("Adivina un numero"))
+if number_to_guess == user_number:
+    print("Has ganado")
+else:
+    user_number = int(input("Adivina un numero"))
+
+    if number_to_guess == user_number:
+        print("Has ganado")
+
+    else:
+        user_number = int(input("Adivina un numero"))
+
+        if number_to_guess == user_number:
+            print("Has ganado")
+        else:
+            user_number = int(input("Adivina un numero"))
+
+            if number_to_guess == user_number:
+                print("Has ganado")
+            else:
+                user_number = int(input("Adivina un numero"))
+                if number_to_guess == user_number:
+                    print("Has ganado")
+                else: print("Has perdido")
